108e2b4e8e01f984071fd54794ccc69c19cd8430
adamcoop <adam@stripysock.com.au>
2023-08-02 14:35:29 +1000

    Add swift-tools-version specific package file, visionOS support

---
+++ Package@swift-5.9.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "NPGKit",
+    platforms: [
+            .macOS(.v11),
+            .iOS(.v13),
+            .visionOS("1.0.0")
+        ],
+    products: [
+        .library(
+            name: "NPGKit",
+            targets: ["NPGKit"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "NPGKit",
+            dependencies: []),
+        .testTarget(
+            name: "NPGKitTests",
+            dependencies: ["NPGKit"]),
+    ]
+)
