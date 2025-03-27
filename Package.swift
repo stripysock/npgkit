ded5aeaa13080152e2182ffbd335e5e6e2b67149
adamcoop <adam@stripysock.com.au>
2025-03-27 19:39:03 +1100

    Make Swift 5.9 minimum tools version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -7,7 +7,8 @@ let package = Package(
     name: "NPGKit",
     platforms: [
             .macOS(.v11),
-            .iOS(.v13)
+            .iOS(.v13),
+            .visionOS(.v1)
         ],
     products: [
         .library(
